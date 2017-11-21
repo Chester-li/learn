d4e3f584b5af48f7a4e60cb0f31fe2e766a9f786
chaoshuai.li <lcs_cust@126.com>
2017-11-21 17:58:19 +0800

    add movie get

---
+++ resteasy/src/main/java/rest/model/Movie.java
@@ -0,0 +1,42 @@
+package rest.model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by chaoshuai.li on 2017/11/21.
+ */
+@XmlRootElement(name = "movie")
+public class Movie {
+    String name;
+    String director;
+    int year;
+
+    @XmlElement
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @XmlElement
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @XmlAttribute
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- resteasy/src/main/java/rest/service/JsonService.java
+++ resteasy/src/main/java/rest/service/JsonService.java
@@ -1,5 +1,7 @@
 package rest.service;
 
+import org.jboss.resteasy.annotations.providers.jaxb.json.BadgerFish;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,32 +9,44 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import rest.model.Movie;
 import rest.model.Product;
 
 @Path("/json/product")
 public class JSONService {
 
-	@GET
-	@Path("/get")
-	@Produces("application/json")
-	public Product getProductInJSON() {
+    //当有两path的value一样的时候使用的是第一个path
+    @GET
+    @Path("/get")
+    @Produces("application/json")
+    public Product getProductInJSON() {
 
-		Product product = new Product();
-		product.setName("iPad 3");
-		product.setId(999);
-		
-		return product; 
+        Product product = new Product();
+        product.setName("iPad 3");
+        product.setId(999);
 
-	}
+        return product;
 
-	@POST
-	@Path("/post")
-	@Consumes("application/json")
-	public Response createProductInJSON(Product product) {
+    }
 
-		String result = "Product created : " + product;
-		return Response.status(201).entity(result).build();
-		
-	}
-	
+    @POST
+    @Path("/post")
+    @Consumes("application/json")
+    public Response createProductInJSON(Product product) {
+        String result = "Product created : " + product;
+        return Response.status(201).entity(result).build();
+    }
+
+    @BadgerFish
+    @GET
+    @Path("/movie/get")
+    @Produces("application/json")
+    public Movie getMovieInJSON() {
+        Movie movie = new Movie();
+        movie.setName("Transformers: Dark of the Moon");
+        movie.setDirector("Michael Bay");
+        movie.setYear(2011);
+        return movie;
+    }
+
 }
