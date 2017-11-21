42fcaf7509d16d7d1e4ee16b68dcb7ff215757b3
chaoshuai.li <lcs_cust@126.com>
2017-11-21 17:50:42 +0800

    support json

--- resteasy/src/main/java/rest/model/User.java
+++ resteasy/src/main/java/rest/model/User.java
@@ -1,4 +1,4 @@
-package rest;
+package rest.model;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
--- resteasy/src/main/java/rest/service/XMLService.java
+++ resteasy/src/main/java/rest/service/XMLService.java
@@ -1,8 +1,10 @@
-package rest;
+package rest.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import rest.model.User;
 
 @Path("/xml/user")
 public class XMLService {
